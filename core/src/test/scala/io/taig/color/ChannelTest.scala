f9b249273c0f3930a39e2d23458c29cdccc8a007
Niklas Klein <mail@taig.io>
2019-11-06 18:14:47 +0100

    Add Channel tests

---
+++ core/src/test/scala/io/taig/color/ChannelTest.scala
@@ -0,0 +1,53 @@
+package io.taig.color
+
+import io.taig.testf.{AutoTest, IOAutoTestApp}
+import io.taig.testf._
+import io.taig.color.dsl._
+
+@AutoTest
+object ChannelTest extends IOAutoTestApp {
+  test("fromScale")(
+    test("-1") {
+      isEqual(Channel.MinValue)(Channel.fromScale(-1f))
+    },
+    test("0") {
+      isEqual(Channel.MinValue)(Channel.fromScale(0f))
+    },
+    test("0.5") {
+      isEqual(Channel.unsafeFromLong(127))(Channel.fromScale(.5f))
+    },
+    test("1") {
+      isEqual(Channel.MaxValue)(Channel.fromScale(1f))
+    },
+    test("2") {
+      isEqual(Channel.MaxValue)(Channel.fromScale(2f))
+    }
+  )
+
+  test("fromHex")(
+    test("-1") {
+      fromLeft(Channel.fromHex(-1))
+    },
+    test("0x00") {
+      fromRight(Channel.fromHex(0x00))
+    },
+    test("0xF0") {
+      fromRight(Channel.fromHex(0xF0))
+    },
+    test("0xFF") {
+      fromRight(Channel.fromHex(0xFF))
+    },
+    test("0xFFF") {
+      fromLeft(Channel.fromHex(0xFFF))
+    }
+  )
+
+  test("parseHex")(
+    test("0x00") {
+      fromRight(Channel.parseHex("FF"))
+    },
+    test("foobar") {
+      fromLeft(Channel.parseHex("foobar"))
+    }
+  )
+}
--- core/src/test/scala/io/taig/color/dsl.scala
+++ core/src/test/scala/io/taig/color/dsl.scala
@@ -4,6 +4,8 @@ import cats.{Eq, Show}
 import io.taig.testf.{Assertions, Builders}
 
 object dsl extends Builders with Assertions {
+  implicit val eqChannel: Eq[Channel] = Eq.fromUniversalEquals
+  implicit val showChannel: Show[Channel] = Show.fromToString
   implicit val eqColor: Eq[Color] = Eq.fromUniversalEquals
   implicit val showColor: Show[Color] = Show.fromToString
 }
