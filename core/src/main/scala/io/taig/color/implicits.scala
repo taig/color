21d0384d9c16110c798384529aa7539226e44d34
Niklas Klein <mail@taig.io>
2019-11-06 15:30:05 +0100

    Move rgb String interpolator to implicits

---
+++ core/src/main/scala/io/taig/color/implicits.scala
@@ -0,0 +1,34 @@
+package io.taig.color
+
+import scala.language.experimental.macros
+import scala.reflect.macros.blackbox
+
+object implicits {
+  implicit class ColorInterpolator(val context: StringContext) extends AnyVal {
+    def rgb(arguments: Any*): Color = macro ColorInterpolator.rgb_impl
+  }
+
+  object ColorInterpolator {
+    def rgb_impl(
+        context: blackbox.Context
+    )(arguments: context.Expr[Any]*): context.Expr[Color] = {
+      import context.universe._
+
+      context.prefix.tree match {
+        case Apply(
+            _,
+            List(Apply(_, List(literal @ Literal(Constant(value: String)))))
+            ) =>
+          Color.parseHex(value) match {
+            case Right(_) =>
+              val expression: context.Expr[String] = context.Expr(literal)
+              reify(Color.unsafeParseHex(expression.splice))
+            case Left(value) =>
+              context.abort(context.enclosingPosition, value)
+          }
+        case _ =>
+          context.abort(context.enclosingPosition, "Invalid color value")
+      }
+    }
+  }
+}
